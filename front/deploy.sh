d83bc6157af5153dff699c371c49bb1c49057d84
Romain Beaumont <romain.rom1@gmail.com>
2021-06-06 23:58:12 +0200

    Delete deploy.sh

--- front/deploy.sh
+++
@@ -1,5 +0,0 @@
-rm -f foag_front.tar
-npm run build
-tar cf dalle-service_front.tar build
-hdfs dfs -rm viewfs://preprod-am6/user/r.beaumont/dalle-service_front.tar
-hdfs dfs -copyFromLocal dalle-service_front.tar viewfs://preprod-am6/user/r.beaumont/dalle-service_front.tar
